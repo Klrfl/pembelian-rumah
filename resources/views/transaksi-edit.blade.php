5472aac94dc16b39ae720557798ed7b996b6ac4b
Efraim Munthe <efrayanglain@gmail.com>
2024-05-25 21:54:32 +0700

    display type rumah in edit form

--- resources/views/transaksi-edit.blade.php
+++ resources/views/transaksi-edit.blade.php
@@ -11,9 +11,9 @@
             <input type="text" name="nama_pembeli" id="nama-pembeli" value="{{$transaksi->nama_pembeli}}" required>
             <label for="tipe">Tipe</label>
             <select name="id_type" id="tipe" required>
-                <option disabled selected>Pilih satu</option>
+                <option disabled>Pilih satu</option>
                 @foreach ($all_type_rumah as $type_rumah)
-                <option value="{{$type_rumah->id_type}}">{{ $type_rumah->type_rumah }}</option>
+                <option value="{{$type_rumah->id_type}}" {!! $type_rumah->id_type === $transaksi->typeRumah->id_type ? 'selected' : '' !!}>{{ $type_rumah->type_rumah }}</option>
                 @endforeach
             </select>
             <label for="luas-tanah">Luas tanah</label>
