64257e85a24f2be5a584fddf2670d10f7941d421
Efraim Munthe <efrayanglain@gmail.com>
2024-05-25 12:15:01 +0700

    display pembelian rumah on home page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,15 +3,9 @@
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae necessitatibus error ut animi saepe totam assumenda illum repellendus consequatur voluptas in distinctio minus quidem excepturi, nostrum magnam inventore ullam consequuntur.</p>
 
     <ul>
-        @foreach ($all_type_rumah as $type_rumah)
+        @foreach ($all_pembelian_rumah as $pembelian_rumah)
         <li>
-            <span>
-                {{ $type_rumah->id_type }}
-            </span>
-            -
-            <span>
-                {{ $type_rumah->type_rumah }}
-            </span>
+            {{ $pembelian_rumah}}
         </li>
         @endforeach
     </ul>
