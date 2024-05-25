32d4799b46e4628ada52dab7ba5d57a485f0490c
Efraim Munthe <efrayanglain@gmail.com>
2024-05-25 11:15:24 +0700

    make navbar

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul class="flex bg-slate-200">
+        <li> <a class="nav-link" href="/">Beranda</a> </li>
+        <li> <a class="nav-link" href="/beli">Beli</a> </li>
+        <li> <a class="nav-link" href="/ubah">Ubah</a> </li>
+        <li> <a class="nav-link" href="/batal">Batal</a> </li>
+    </ul>
+</nav>
