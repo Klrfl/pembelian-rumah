f022221d99e5fc7b222262295fa35df45b507fda
Efraim Munthe <efrayanglain@gmail.com>
2024-05-25 12:15:06 +0700

    create proper form

--- resources/views/beli.blade.php
+++ resources/views/beli.blade.php
@@ -3,24 +3,25 @@
     <p>Halaman untuk membeli rumah (INSERT).</p>
     <p>bentar ya formnya belum jadi</p>
 
-    <form class="p-4 border-2 border-solid border-slate-200 max-w-4xl">
+    <form class="p-4 border-2 border-solid border-slate-200 max-w-4xl" action="/beli" method="post">
+        @csrf
         <label for="kode-unit">Kode unit</label>
-        <input type="text" name="kode-unit" id="kode-unit" required>
+        <input type="text" name="kode_unit" id="kode-unit" required>
         <label for="nama-pembeli">Nama pembeli</label>
-        <input type="text" name="nama-pembeli" id="nama-pembeli" required>
+        <input type="text" name="nama_pembeli" id="nama-pembeli" required>
         <label for="tipe">Tipe</label>
-        <select name="tipe" id="tipe" required>
+        <select name="id_type" id="tipe" required>
             <option disabled selected>Pilih satu</option>
             @foreach ($all_type_rumah as $type_rumah)
             <option value="{{$type_rumah->id_type}}">{{ $type_rumah->type_rumah }}</option>
             @endforeach
         </select>
         <label for="luas-tanah">Luas tanah</label>
-        <input type="text" name="luas-tanah" id="luas-tanah" required>
+        <input type="text" name="luas_tanah" id="luas-tanah" required>
         <label for="luas-bangunan">Luas bangunan</label>
-        <input type="text" name="luas-bangunan" id="luas-bangunan" required>
+        <input type="text" name="luas_bangunan" id="luas-bangunan" required>
         <label for="harga-rumah">Harga rumah</label>
-        <input type="text" name="harga-rumah" id="harga-rumah" required>
+        <input type="text" name="harga_rumah" id="harga-rumah" required>
 
         <button class="p-4 my-4 bg-blue-500 text-white">Beli rumah</button>
     </form>
