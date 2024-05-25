98dfccd07f17adf17f1afdc10986edd8085db60f
Efraim Munthe <efrayanglain@gmail.com>
2024-05-25 22:13:59 +0700

    change all routes to named routes

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav>
     <ul class="flex bg-slate-200">
         <li> <a class="nav-link" href="/">Beranda</a> </li>
-        <li> <a class="nav-link" href="/beli">Beli</a> </li>
+        <li> <a class="nav-link" href="{{route('beli.show')}}">Beli</a> </li>
     </ul>
 </nav>
--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ Route::get('/', function () {
     return view('index', ['all_pembelian_rumah' => $all_pembelian_rumah]);
 });
 
-Route::get('/beli', [PembelianRumahController::class, 'index'])->name('beli,show');
+Route::get('/beli', [PembelianRumahController::class, 'index'])->name('beli.show');
 Route::post('/beli', [PembelianRumahController::class, 'create'])->name('beli.update');
 Route::get('/transaksi/{id}', [PembelianRumahController::class, 'show'])->name('transaksi.show');
 Route::get('/transaksi/{id}/edit', [PembelianRumahController::class, 'showUpdate'])->name('transaksi.show-update');
