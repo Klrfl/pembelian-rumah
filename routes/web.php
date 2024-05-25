e5b06bb5374f516f82404fe83f8e13a478f9ed63
Efraim Munthe <efrayanglain@gmail.com>
2024-05-25 10:46:47 +0700

    make base layout, change default landing page, display type_rumahs to test ORM

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\TypeRumah;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $all_type_rumah = TypeRumah::all();
+    return view('index', ['all_type_rumah' => $all_type_rumah]);
 });
