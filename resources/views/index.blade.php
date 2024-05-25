60fad8a2cdbaefa644605c52ba9118968a7f3b3c
Efraim Munthe <efrayanglain@gmail.com>
2024-05-25 21:57:34 +0700

    add margin

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae necessitatibus error ut animi saepe totam assumenda illum repellendus consequatur voluptas in distinctio minus quidem excepturi, nostrum magnam inventore ullam consequuntur.</p>
     </header>
 
-    <ul class="flex flex-col gap-4 max-w-4xl">
+    <ul class="flex flex-col gap-4 max-w-4xl mt-4">
         @foreach ($all_pembelian_rumah as $pembelian_rumah)
         <x-pembelian-rumah :transaksi="$pembelian_rumah" />
         @endforeach
