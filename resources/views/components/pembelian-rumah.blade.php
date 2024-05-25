ca7ea5cba3f28843f00eea415e43a4e742f6c971
Efraim Munthe <efrayanglain@gmail.com>
2024-05-25 21:48:54 +0700

    display type rumah

--- resources/views/components/pembelian-rumah.blade.php
+++ resources/views/components/pembelian-rumah.blade.php
@@ -3,7 +3,7 @@
         <p>ID: {{$transaksi->id_transaksi}}</p>
         <p>Kode unit: {{$transaksi->kode_unit}}</p>
         <p>Nama pembeli: {{$transaksi->nama_pembeli}}</p>
-        <p>Tipe: {{$transaksi->id_type}}</p>
+        <p>Tipe: {{$transaksi->typeRumah->type_rumah}}</p>
         <p>Luas tanah: {{$transaksi->luas_tanah}}</p>
         <p>Luas bangunan: {{$transaksi->luas_bangunan}}</p>
         <p>Harga rumah: {{$transaksi->harga_rumah}}</p>
