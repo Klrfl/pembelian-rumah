5ffa905dce8951cf204f2eba704d1ff348a3ff79
Efraim Munthe <efrayanglain@gmail.com>
2024-05-26 12:14:22 +0700

    create migrations for type_rumah and pembelian_rumah tables

---
+++ database/migrations/2024_05_26_045525_create_pembelian_rumah_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::dropIfExists('pembelian_rumah');
+
+        Schema::create('pembelian_rumah', function (Blueprint $table) {
+            $table->id('id_transaksi');
+            $table->integer('kode_unit');
+            $table->string('nama_pembeli');
+            $table->foreign('id_type')->references('id_type')->on('type_rumah');
+            $table->string('luas_tanah');
+            $table->string('luas_bangunan');
+            $table->decimal('harga_rumah');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pembelian_rumah');
+    }
+};
